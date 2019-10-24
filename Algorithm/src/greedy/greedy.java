8421e12164ec49d3e1563cd44991f57be6be6d23
김현우 <@192.168.219.104>
2019-10-24 11:20:39 +0900

    greedy

---
+++ Algorithm/src/greedy/greedy.java
@@ -0,0 +1,24 @@
+package greedy;
+
+public class greedy {
+	    public int solution(int n, int[] lost, int[] reserve) {
+	        int answer = 0;
+	        answer = n;
+	        
+	        
+	        for(int i = 0; i < lost.length;i++ ) {
+	        	boolean rent = false;
+	        	int j = 0;
+	        	while(!rent) {
+	        		if(j == reserve.length) break;
+	        		if(lost[i]==reserve[j]) {reserve[j] = -1;rent = true;}
+	        		else if(lost[i]- reserve[j] == 1) {reserve[j] = -1; rent = true;}
+	        		else if(lost[i]-reserve[j] == -1) {reserve[j] = -1; rent = true;}
+	        		else {j++;}
+	        	}
+	        	if(!rent) answer --;
+	        }
+	        return answer;
+	        }
+	}
+	
