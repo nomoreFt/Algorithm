55bf281f896cacc9689faf2c8e6261833aca97ce
hyunwoo.kim <hyunwoo.kim1108@gmail.com>
2020-12-10 22:59:47 +0900

    DFS 공부

---
+++ Algorithm/src/BAEKJOON/BruteForce/_14500.java
@@ -0,0 +1,21 @@
+package BAEKJOON.BruteForce;
+
+import java.io.*;
+
+public class _14500 {
+	static int N;
+	static int M;
+	static BufferedReader br;
+	static int[][] arr;
+	static boolean[][] visited;
+	static int[] dx = { -1, 0, 1, 0};
+	static int[] dy = {0, -1, 0, 1};
+	public static void main(String[] args) throws IOException{
+		br = new BufferedReader(new InputStreamReader(System.in));
+		String[] str = br.readLine().split(" ");
+		N = Integer.parseInt(str[0]);
+		M = Integer.parseInt(str[1]);
+		
+		
+	}
+}
