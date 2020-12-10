02480cb5c3a8978cf39ff8689c40054aa1a9aebb
nomoreFt <hyunwoo.kim1108@gmail.com>
2020-12-11 00:55:19 +0900

    문제 해결

--- Algorithm/src/BAEKJOON/Math/_6588.java
+++ Algorithm/src/BAEKJOON/Math/_6588.java
@@ -44,29 +44,4 @@ public class _6588 {
 		}
 		System.out.println(sb.toString());
 	}
-	
-	
-	/*
-	 * public static void main(String[] args) throws IOException { BufferedReader br
-	 * = new BufferedReader(new InputStreamReader(System.in)); StringBuilder sb =
-	 * new StringBuilder(); final int N = 1000000; boolean[] b = new boolean[N + 1];
-	 * List<Integer> list = new ArrayList<>();
-	 * 
-	 * Arrays.fill(b, true);
-	 * 
-	 * for (int i = 3; i * i <= N; i += 2) { if (b[i]) { for (int j = i * i; j <= N;
-	 * j += i) { b[j] = false; } } } for (int i = 3; i <= N; i += 2) { if (b[i])
-	 * list.add(i); }
-	 * 
-	 * while (true) { int val = Integer.parseInt(br.readLine()); if (val == 0)
-	 * break; boolean isWrong = true;
-	 * 
-	 * for (int i = 0; i < list.size(); i++) { if (b[val - (list.get(i))]) { String
-	 * str = String.valueOf(val) + " = " + String.valueOf(list.get(i)) + " + " +
-	 * String.valueOf(val - list.get(i)) + "\n"; sb.append(str); isWrong = false;
-	 * break; } } if (isWrong) { sb.append("Goldbach's conjecture is wrong."); } }
-	 * System.out.println(sb.toString());
-	 * 
-	 * }
-	 */
 }
