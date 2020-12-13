fba17b39794f0c411ef267730b7b47642e209b19
nomoreFt <hyunwoo.kim1108@gmail.com>
2020-12-13 23:28:16 +0900

    9095

---
+++ Algorithm/src/BAEKJOON/BruteForce/_9095.java
@@ -0,0 +1,26 @@
+package BAEKJOON.BruteForce;
+
+import java.io.*;
+
+public class _9095 {
+	static BufferedReader br;
+	static StringBuilder sb;
+	static int val;
+	static int[] arr;
+	static int cnt = 0;
+	public static void main(String[] args) throws IOException{
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		val = Integer.parseInt(br.readLine());
+		arr = new int[val];
+		for(int i = 0 ; i < val; i++) {
+			 int a = Integer.parseInt(br.readLine());
+			 for(int j = 1; j < a; j++) {
+				 for(int k = 1; j < a; k++) {
+					 
+				 }
+			 }
+		}
+	}
+
+}
