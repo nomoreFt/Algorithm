64af9a3429cde6df66816af46942981673d1891b
nomoreFt <hyunwoo.1108@gmail.com>
2020-12-19 01:54:50 +0900

    10971 외판원 마무리

--- Algorithm/src/BAEKJOON/BruteForce/Permutation/_10971.java
+++ Algorithm/src/BAEKJOON/BruteForce/Permutation/_10971.java
@@ -1,40 +1,47 @@
 package BAEKJOON.BruteForce.Permutation;
 
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
 public class _10971 {
-	static int[][] price;
-	static int N;
-	static int[] arr;
-	static boolean[] visited;
-	static int min = Integer.MAX_VALUE;
-	static int sum;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		price = new int[N][N];
-		for(int i = 0 ; i < N; i++) {
-			String[] strs = br.readLine().split(" ");
-			for(int j = 0 ; j < N; j++) {
-				price[i][j] = Integer.parseInt(strs[j]);
-			}
-		}
-		arr = new int[N];
-		visited = new boolean[N];
-		permutation(arr,visited,0);
-		System.out.println(min);
-	}
-	public static void permutation(int[] arr, boolean[] visited, int depth) {
-		if(depth == N) {
-			min = Math.min(sum, min);
-			return;
-		}
-		for(int i = 0 ; i < N; i++) {
-			if(visited[i]) continue;
-			sum += arr[i];
-			visited[i] = true;
-			permutation(arr,visited,depth+1);
-			sum -= arr[i];
-		}
-	}
+ 
+    static int n ;
+    static int[][] arr;
+    static long min = Integer.MAX_VALUE;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
+        visited = new boolean[n];
+ 
+         for (int i = 0; i <n ; i++) {
+        	 String[] str = br.readLine().split(" ");
+             for (int j = 0; j <n ; j++) {
+                 arr[i][j] = Integer.parseInt(str[j]);
+            }
+        }
+        for (int i = 0; i <n ; i++) {
+            dfs(i,i,0,0);
+        }
+        System.out.println(min);
+    }
+    public static void dfs(int start, int i, int depth, int sum){
+        if(depth == n && start == i){
+            min = Math.min(min, sum);
+            return;
+        } 
+        for (int j = 0; j <n ; j++) {
+            if(arr[i][j]==0 || visited[i]){
+                continue;
+            }
+                visited[i]=true;
+                sum += arr[i][j];
+                dfs(start, j, depth +1, sum);
+                visited[i] = false;
+                sum -= arr[i][j];
+        }
+    }
+ 
 }
