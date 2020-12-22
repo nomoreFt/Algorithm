6575523ff1a1d3b710d898e9c04b8177d388bdcb
nomoreFt <hyunwoo.kim1108@gmail.com>
2020-12-22 23:32:56 +0900

    dfs

--- Algorithm/src/BAEKJOON/BruteForce/Permutation/_6603.java
+++ Algorithm/src/BAEKJOON/BruteForce/Permutation/_6603.java
@@ -2,50 +2,49 @@ package BAEKJOON.BruteForce.Permutation;
 
 import java.io.*;
 
+
 public class _6603 {
 	static int N;
-	static int[] arr; 
-	static int[] output;
-	static int lottoNum = 6;
-	static String[] line;
-	static StringBuilder sb;
+	static int arr[];
+	static int output[];
 	static boolean visited[];
+	static int lottoNum = 6;
+	static StringBuilder sb;
 	public static void main(String[] args) throws IOException{
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		sb = new StringBuilder();
 		while(true) {
-			line = br.readLine().split(" ");
-			N = Integer.parseInt(line[0]);
-			if(Integer.parseInt(line[0])==0) break;
-			arr = new int[N+1];
-			for(int i = 1 ; i <=N; i++) {
-				arr[i] = Integer.parseInt(line[i]);
+			String[] strs = br.readLine().split(" ");
+			N = Integer.parseInt(strs[0]);
+			if(N == 0) break;
+			arr = new int[N];
+			output = new int[lottoNum];
+			visited = new boolean[N];
+			for(int i = 1 ; i <= N; i++) {
+				arr[i-1] = Integer.parseInt(strs[i]);
 			}
-			output = new int[N+1];
-			visited = new boolean[N+1];
-			permutation(arr,output,0,visited);
+			dfs(0);
 			sb.append("\n");
 		}
 		System.out.println(sb.toString());
 	}
-	public static void permutation(int[] arr, int[] output, int depth, boolean[] visited) {
-		if(depth==lottoNum) {
+	public static void dfs(int depth) {
+		if(depth == lottoNum) {
 			print(output);
 			return;
 		}
-		for(int i = 1; i <=N; i++) {
+		for(int i = 0; i < N; i++) {
 			if(visited[i]) continue;
 			visited[i] = true;
 			output[depth] = arr[i];
-			permutation(arr,output,depth+1,visited);
+			dfs(depth+1);
 			visited[i] = false;
 		}
 	}
 	public static void print(int[] output) {
-		for(int i = 1 ; i <= N ; i++) {
-			sb.append(output[i] + " ");
+		for(int i = 0 ; i < output.length; i++) {
+			if(i == output.length-1) sb.append(output[i]+"\n");
+			else sb.append(output[i] + " ");
 		}
-		sb.append("\n");
 	}
 }
