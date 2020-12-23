aaa7cdacb55ee3d53f8736c7f9a9974b01e0837c
hyunwoo.kim <hyunwoo.kim1108@gmail.com>
2020-12-23 23:08:11 +0900

    dfs

--- Algorithm/src/BAEKJOON/BruteForce/Permutation/_6603.java
+++ Algorithm/src/BAEKJOON/BruteForce/Permutation/_6603.java
@@ -6,19 +6,15 @@ import java.io.*;
 public class _6603 {
 	static int N;
 	static int arr[];
-	static int output[];
 	static boolean visited[];
-	static int lottoNum = 6;
-	static StringBuilder sb;
+	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		sb = new StringBuilder();
 		while(true) {
 			String[] strs = br.readLine().split(" ");
 			N = Integer.parseInt(strs[0]);
 			if(N == 0) break;
 			arr = new int[N];
-			output = new int[lottoNum];
 			visited = new boolean[N];
 			for(int i = 1 ; i <= N; i++) {
 				arr[i-1] = Integer.parseInt(strs[i]);
@@ -29,14 +25,12 @@ public class _6603 {
 		System.out.println(sb.toString());
 	}
 	public static void dfs(int depth) {
-		if(depth == lottoNum) {
-			print(output);
+		if(depth == 6) {
 			return;
 		}
 		for(int i = 0; i < N; i++) {
 			if(visited[i]) continue;
 			visited[i] = true;
-			output[depth] = arr[i];
 			dfs(depth+1);
 			visited[i] = false;
 		}
