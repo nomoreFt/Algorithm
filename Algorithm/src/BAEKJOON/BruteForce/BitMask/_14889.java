e62e98e58e3ff56b5a9a7af8bb822ec441bc1437
hyunwoo.kim <hyunwoo.kim1108@gmail.com>
2021-01-18 17:57:50 +0900

    dfs

--- Algorithm/src/BAEKJOON/BruteForce/BitMask/_14889.java
+++ Algorithm/src/BAEKJOON/BruteForce/BitMask/_14889.java
@@ -4,7 +4,6 @@ import java.io.*;
 
 public class _14889 {
 	static int N,min = Integer.MIN_VALUE;
-	static int startSum=0,linkSum=0;
 	static boolean[] visited;
 	static int[][] S;
 	
@@ -12,6 +11,7 @@ public class _14889 {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(bf.readLine());
 		S = new int[N][N];
+		visited = new boolean[N];
 		for(int i = 0; i < N; i++) {
 			String[] strs = bf.readLine().split(" ");
 			for (int j = 0; j < N; j++) {
@@ -33,13 +33,29 @@ public class _14889 {
 		}
 	}
 	public static void valid() {
+		int team_start = 0;
+		int team_link = 0;
 		
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				if(visited[i]&&visited[j]) {
-					startSum+=S[i][j];
+		for(int i = 0; i < N-1; i++) {
+			for(int j = i+1; j < N; j++) {
+				if(visited[i]==true&&visited[j]==true) {
+					team_start += S[i][j];
+					team_start += S[j][i];
+				}
+				if(visited[i]==false && visited[j] == false) {
+					team_link+=S[i][j];
+					team_link+=S[j][i];
 				}
 			}
 		}
+		
+		int val = Math.abs(team_start-team_link);
+		
+		if(val == 0) {
+			System.out.println(val);
+			System.exit(0);
+		}
+		
+		min = Math.min(val, min);
 	}
 }
