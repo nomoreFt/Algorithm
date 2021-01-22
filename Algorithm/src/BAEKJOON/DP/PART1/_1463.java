584334a8b374c1c96aedfb5c2fda3d9ab903ee88
hyunwoo.kim <hyunwoo.kim1108@gmail.com>
2021-01-22 22:55:57 +0900

    dp

--- Algorithm/src/BAEKJOON/DP/PART1/_1463.java
+++ Algorithm/src/BAEKJOON/DP/PART1/_1463.java
@@ -1,5 +1,10 @@
 package BAEKJOON.DP.PART1;
 
+import java.util.Scanner;
 public class _1463 {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		
+	}
 }
