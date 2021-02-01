7a93bd28d76fe4a5b3c6b35db26454539a90757f
hyunwoo.kim <hyunwoo.kim1108@gmail.com>
2021-02-01 22:36:11 +0900

    dp

--- Algorithm/src/BAEKJOON/DP/PART1/_9095.java
+++ Algorithm/src/BAEKJOON/DP/PART1/_9095.java
@@ -3,23 +3,24 @@ package BAEKJOON.DP.PART1;
 import java.io.*;
 
 public class _9095 {
-	static StringBuilder sb = new StringBuilder();
+	static int n;
 	static int[] d;
+	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException{
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(bf.readLine());
+		n = Integer.parseInt(bf.readLine());
 		d = new int[11];
-		while(T-- > 0) {
-			int n = Integer.parseInt(bf.readLine());
-			sb.append(dp(n)+"\n");
+		while(n-- > 0) {
+			int x = Integer.parseInt(bf.readLine());
+			sb.append(dp(x)+"\n");
 		}
 		System.out.println(sb.toString());
 	}
 	static int dp(int x) {
-		if(x == 1) return 1;
-		if(x == 2) return 2;
-		if(x == 3) return 4;
-		if(d[x] != 0) return d[x];
-		return d[x] = dp(x-1) + dp(x-2) + dp(x-3);
+		if(x==1) return 1;
+		if(x==2) return 2;
+		if(x==3) return 4;
+		if(d[x]!=0) return d[x];
+		return d[x] = dp(x-1) + dp(x-2)+ dp(x-3);
 	}
 }
