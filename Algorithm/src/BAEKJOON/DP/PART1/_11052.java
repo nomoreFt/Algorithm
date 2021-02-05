5a8d37b0bccd5da690874343cdaeef658865bde7
hyunwoo.kim <hyunwoo.kim1108@gmail.com>
2021-02-05 17:59:17 +0900

    dp

--- Algorithm/src/BAEKJOON/DP/PART1/_11052.java
+++ Algorithm/src/BAEKJOON/DP/PART1/_11052.java
@@ -2,29 +2,26 @@ package BAEKJOON.DP.PART1;
 
 import java.io.*;
 
-public class _11052 {
+public class _11052{
 	static int N;
-	static int[] d;
-	static int[] P;
+	static int[] P,d;
 	public static void main(String[] args) throws IOException{
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(bf.readLine());
-		d = new int[N+1];
 		P = new int[N+1];
+		d = new int[N+1];
 		String[] strs = bf.readLine().split(" ");
-		for(int i = 1; i <= N; i++) {
-			P[i] = Integer.parseInt(strs[i-1]);
+		for(int i = 0; i < N; i++) {
+			P[i+1] = Integer.parseInt(strs[i]);
 		}
 		System.out.println(dp(N));
 	}
 	static int dp(int x) {
-		if(x == 1) return P[1];
-		if(x == 2) return Math.max(P[2], dp(1)*2);
+		if(x==0) return 0;
 		if(d[x] != 0) return d[x];
-		int result = 0;
-		for(int i = 1; i < x; i++) {
-			d[x] = Math.max(P[x], dp(x-i) + dp(i));
-	}
+		for(int i = 1; i <= x; i++) {
+			d[x] = Math.min(d[x], dp(x-i) + P[i]);
+		}
 		return d[x];
 	}
 }
---
+++ Algorithm/src/BAEKJOON/DP/PART1/_16194.java
@@ -0,0 +1,11 @@
+package BAEKJOON.DP.PART1;
+
+import java.io.*;
+
+public class _16194 {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
