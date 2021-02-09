40484f1669c7a86e3d2f3ffb9cdc6e1e195c8650
hyunwoo.kim <hyunwoo.kim1108@gmail.com>
2021-02-09 23:00:20 +0900

    dp

--- Algorithm/src/BAEKJOON/DP/PART1/_15990.java
+++ Algorithm/src/BAEKJOON/DP/PART1/_15990.java
@@ -1,35 +1,37 @@
 package BAEKJOON.DP.PART1;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class _15990 {
-	static int n,div,limit;
-	static StringBuilder sb = new StringBuilder();
+public class _15990{
+	static int T,n,div,limit;
 	static int[][] d;
 	public static void main(String[] args) throws IOException{
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(bf.readLine());
-		div = 1000000009;
-	
-		while(n-- > 0) {
-			int x = Integer.parseInt(bf.readLine());
-			d = new int[x+1][4];
+        StringBuilder sb = new StringBuilder();
+        div = 1000000009; 
+        limit = 100000;
+        T = Integer.parseInt(bf.readLine().trim());
+		d = new int [limit+1][4];
+		d[1][1] = d[2][2] = d[3][1] = d[3][2] = d[3][3] = 1;
+		for(int i = 4; i <= limit; i++) {
+			for(int j = 1; j <= 3; j++) {
+				if(j == 1) d[i][1] = d[i-1][2] + d[i-1][3]; 
+				else if (j == 2) d[i][2] = d[i-2][1] + d[i-2][3];
+				else d[i][j] = d[i-3][3] + d[i-3][2];
+				d[i][j] %= div;
+			}
+		}
+		for(int tc = 1; tc <= T; tc++){
+			n = Integer.parseInt(bf.readLine().trim());
 			int result = 0;
 			for(int i = 1; i <= 3; i++) {
-				result += dp(x,i)%div;
-			} 
+				result += d[n][i];
+                result %= div;
+			}
 			sb.append(result+"\n");
 		}
 		System.out.println(sb.toString());
 	}
-	static int dp(int x, int y) {
-		if(x==1 && y == 1) return d[1][1]=1;
-		if(x==2 && y == 2) return d[2][2]=1;
-		if(x==3 && y==1 || y == 2 || y == 3) return d[3][1]=d[3][2]=d[3][3]=1;
-		if(d[x][y] != 0) return d[x][y];
-		if(y == 1) return d[x][y] = dp(x-1,2) + dp(x-1,3);
-		else if(y == 2) return d[x][y] = dp(x-2,1)  +dp(x-2,3);
-		else return d[x][y] = dp(x-3,1) + dp(x-3,2);
-		
-	}
-		}
+}
