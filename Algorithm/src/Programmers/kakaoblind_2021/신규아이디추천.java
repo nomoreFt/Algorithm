6a1d75697509c25459cb4b8d966c00c7be0fb145
hyunwoo.kim <hyunwoo.kim1108@gmail.com>
2021-02-22 23:02:27 +0900

    동빈나

---
+++ Algorithm/src/Programmers/kakaoblind_2021/신규아이디추천.java
@@ -0,0 +1,35 @@
+package Programmers.kakaoblind_2021;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class 신규아이디추천 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String new_id = bf.readLine();
+		
+		System.out.println(solution(new_id));
+	}
+	
+	public static String solution(String new_id) {
+			String answer = "";
+			new_id=new_id.toLowerCase();
+			new_id=new_id.replaceAll("[^a-z0-9-_.]", "");
+			new_id=new_id.replaceAll("\\.{2,}", ".");
+			new_id=new_id.replaceAll("^\\.","");
+			new_id=new_id.replaceAll("[.]$","");
+			if(new_id.isBlank()) {
+				new_id += "a";
+			}
+			if(new_id.length()>=16) {
+				new_id = new_id.substring(0, 15);
+				new_id = new_id.replaceAll("[.]$","");
+			}
+			if(new_id.length() <=2) {
+				while(new_id.length()<3) {
+					new_id += new_id.charAt(new_id.length()-1);
+				}
+			}
+			return new_id;
+	}
+}
---
+++ Algorithm/src/codingTest/DFS_BFS/음료수얼려먹기.java
@@ -0,0 +1,9 @@
+package codingTest.DFS_BFS;
+
+
+
+public class 음료수얼려먹기 {
+	public static void main(String[] args) {
+		
+	}
+}
