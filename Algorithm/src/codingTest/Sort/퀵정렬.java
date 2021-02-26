4f765f6bc4748c1eb26c2571111aa67be930afa8
nomoreFt <hyunwoo.kim1108@gmail.com>
2021-02-26 17:59:28 +0900

    퀵

--- Algorithm/src/codingTest/Sort/퀵정렬.java
+++ Algorithm/src/codingTest/Sort/퀵정렬.java
@@ -4,7 +4,7 @@ package codingTest.Sort;
 //pivot (기준데아터) 를 설정하고 그 기준보다 큰 데이터와 작은 데이터의 위치를 변경해주는 것이다.
 //기본은 첫 데이터를 기준
 //O(NlogN)
-//
+////
 //재귀
 public class 퀵정렬 {
 	public static void main(String[] args) {
