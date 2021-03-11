38d1975ecf251e44282af45ce027559b3419e626
nomoreFt <hyunwoo.kim1108@gmail.com>
2021-03-11 23:05:01 +0900

    이진탐색

--- Algorithm/src/codingTest/탐색/떡볶이떡.java
+++ Algorithm/src/codingTest/탐색/떡볶이떡.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.Arrays;
 
 public class ∂±∫∫¿Ã∂± {
-	static int N, M;
+	static int N, M, H;
 	static int[] arr;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,21 +17,31 @@ public class ∂±∫∫¿Ã∂± {
 			arr[i] = Integer.parseInt(str[i]);
 		}
 		
-		int start = 0;
-		int end =(int) 1e9;
+		Arrays.sort(arr);
 		
-		int result = 0;
-		while( start <= end) {
-			long total = 0;
-			int mid = (start + end) / 2;
-			for(int i = 0 ; i < N; i++) {
-				if(arr[i] > mid) total += arr[i] - mid;
-			}if(total < M) {
-				end = mid - 1;
-			}else {
-				result = mid;
-				start = mid + 1;
-			}
+		int start = 0;
+		int end =arr[arr.length-1];
+		
+		/*
+		 * int result = 0; while( start <= end) { long total = 0; int mid = (start +
+		 * end) / 2; for(int i = 0 ; i < N; i++) { if(arr[i] > mid) total += arr[i] -
+		 * mid; }if(total < M) { end = mid - 1; }else { result = mid; start = mid + 1; }
+		 * }
+		 */	
+		System.out.println(BinarySearch(start,end));
+	}									
+	public static int BinarySearch(int start, int end) {
+		if(start > end ) return -1;
+		long total = 0;
+		H = (start + end) / 2;
+		for(int i = 0 ; i < N; i++) {
+			if(arr[i] > H) total += arr[i] - H; 
 		}
+		if(total == M) {
+			return H;
+		}else if(total < M){
+			return BinarySearch(start, H-1);
+		}else return BinarySearch(H+1,end);
+		
 	}
 }
