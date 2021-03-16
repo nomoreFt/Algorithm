9e9d8b9c3ac66013a44053f2c934e936c1034cd8
nomoreFt <hyunwoo.kim1108@gmail.com>
2021-03-17 00:20:33 +0900

    dddddp

--- Algorithm/src/codingTest/Dp/개미전사.java
+++ Algorithm/src/codingTest/Dp/개미전사.java
@@ -5,25 +5,22 @@ import java.io.*;
 public class 개미전사 {
 	static int N;
 	static int[] K;
-	
+	public static int[] d = new int[100];
 	public static void main(String[] args) throws IOException{
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(bf.readLine());
-		K = new int[N+1];
+		K = new int[N];
 		String[] strs = bf.readLine().split(" ");
-		for(int i = 1; i <= N; i++) {
-			K[i] = Integer.parseInt(strs[i-1]);
+		for(int i = 0; i < N; i++) {
+			K[i] = Integer.parseInt(strs[i]);
 		}
-		int even = 0;
-		int odd = 0;
-		for(int i = 1; i <= N; i++) {
-			if(i%2 == 0) {
-				even += K[i];
-			}else {
-				odd += K[i];
-			}
+		
+		d[0] = K[0];
+		d[1] = Math.max(K[0], K[1]);
+		for(int i = 2; i < N; i++) {
+			d[i] = Math.max(K[i-1], K[i-2]+K[i]);
 		}
-		System.out.println(Math.max(even,odd));
+		System.out.println(d[N-1]);
 	}
 
 }
