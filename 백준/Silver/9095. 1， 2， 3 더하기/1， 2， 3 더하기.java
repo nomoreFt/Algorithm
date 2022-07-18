999c1e22e47684b40e693890af63aae3b7d971d0
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2022-07-18 17:33:07 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,22 +1,28 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
 
-public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int val = Integer.parseInt(br.readLine());
-		int[] arr = new int[11];
-		arr[1] = 1;
-		arr[2] = 2;
-		arr[3] = 4;
-		for(int i = 0; i < val ; i++) {
-			int a = Integer.parseInt(br.readLine());
-			for(int j = 4; j<=a; j++) {
-				if((arr[j]) != 0) continue;
-				arr[j] = arr[j-1]+arr[j-2]+arr[j-3];
-			}
-			sb.append(arr[a]+"\n");
-		}
-		System.out.println(sb.toString());
-	}
+class Main {
+    static int[] dp;
+    static int n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        dp = new int[13];
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+
+            for (int i = 4; i <= 11; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            }
+        while (T-- > 0) {
+            n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n]);
+        }
+    }
 }
+
