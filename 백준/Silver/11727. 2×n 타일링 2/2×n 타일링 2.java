babc033650ba1feed1b797928a00aed811fc3835
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2022-07-18 17:21:43 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 128 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+
+class Main {
+    static int[] dp;
+    static int n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        dp = new int[n + 2];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 3;
+        
+        if(n == 1){
+            System.out.println(1);
+            return;
+        }else if(n == 2){
+            System.out.println(3);
+            return;
+        }
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]*2) % 10007;
+        }
+        System.out.println(dp[n]);
+
+    }
+}
+
