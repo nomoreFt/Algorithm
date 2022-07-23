0a864d500b458b455f18a44534b04c1dbdbc7bea
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2022-07-23 21:35:55 +0900

    [Silver I] Title: 카드 구매하기, Time: 148 ms, Memory: 14504 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    static int[] dp;
+    static int n;
+    static int[] p;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        String[] strs = br.readLine().split(" ");
+        dp = new int[1002];
+        p = new int[1002];
+        int length = strs.length;
+        for (int i = 1; i <= length; i++) {
+            p[i] = Integer.parseInt(strs[i-1]);
+            dp[i] = Integer.parseInt(strs[i-1]);
+        }
+        dp[0] = 0;
+        dp[1] = p[1];
+
+        for (int i = 2; i <= n; i++) {
+            for (int k = 1; k < i; k++) { //i == 4 , k = 1, 2, 3
+                dp[i] = Math.max(dp[i],dp[i - k] + dp[k]);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
+
