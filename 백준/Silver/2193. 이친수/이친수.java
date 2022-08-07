7cf088ca7d1987a4f9f0d857145bbd19ca9d158e
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2022-08-07 20:11:19 +0900

    [Silver III] Title: 이친수, Time: 76 ms, Memory: 11464 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static long[][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        dp = new long[N + 1][2];
+        dp[1][1] = 1;
+        dp[1][0] = 0;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+            dp[i][1] = dp[i - 1][0];
+        }
+        System.out.println(dp[N][0] + dp[N][1]);
+    }
+}
