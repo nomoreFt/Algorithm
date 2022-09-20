60b8c256f7362c6a07a62907272eb3a01d18fcbf
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2022-09-20 23:13:12 +0900

    [Gold IV] Title: 스도쿠, Time: 424 ms, Memory: 22056 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -4,7 +4,18 @@ import java.time.chrono.MinguoChronology;
 import java.util.*;
 
 public class Main {
+    public static class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
     static int[][] graph = new int[9][9];
+    static ArrayList<Node> targets = new ArrayList<>();
+    static int size;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -12,42 +23,36 @@ public class Main {
             String[] strs = br.readLine().split(" ");
             for (int j = 0; j < 9; j++) {
                 graph[i][j] = Integer.parseInt(strs[j]);
+                if(graph[i][j] == 0) targets.add(new Node(i, j));
             }
         }
+        size = targets.size();
 
-        dfs(0, 0);
+        dfs(0);
     }
     //arr[depth열] = 값(행) ex) arr[0] = 1  (0,1)에 체스가 위치
-    static void dfs(int row, int col) {
+    static void dfs(int depth) {
         //depth +1 될 때 마다 왼쪽에서 오른쪽으로 진행이다. 전체 진행 완료란 의미
-        if (col == 9) {
-            dfs(row + 1, 0);
-            return;
-        }
-
-        if (row == 9) {
+        if (depth == size) {
             for (int i = 0; i < 9; i++) {
                 for (int j = 0; j < 9; j++) {
                     System.out.print(graph[i][j] + " ");
                 }
                 System.out.println();
             }
-
             System.exit(0);
         }
 
-        if (graph[row][col] == 0) {
-            for (int i = 1; i <= 9; i++) {
-                if (possibility(row, col, i)) {
-                    graph[row][col] = i;
-                    dfs(row, col + 1);
-                }
+        Node now = targets.get(depth);
+        int nowRow = now.x;
+        int nowCol = now.y;
+        for (int i = 1; i <= 9; i++) {
+            if (possibility(nowRow, nowCol, i)) {
+                graph[nowRow][nowCol] = i;
+                dfs(depth + 1);
+                graph[nowRow][nowCol] = 0;
             }
-            graph[row][col] = 0;
-            return;
         }
-
-        dfs(row, col + 1);
     }
 
     static boolean possibility(int row, int col, int value) {
