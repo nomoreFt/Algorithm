24ad88da72e246e09845c1f46d9c9d80d64504b3
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2022-12-08 19:41:57 +0900

    [Silver III] Title: 사탕 게임, Time: 148 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -1,77 +1,83 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 
 class Main {
-    static String[][] graph;
     static int N;
-    static int ans = 1;
+    static char[][] graph;
+    static int result = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
-        graph = new String[N][N];
+
+        graph = new char[N][N];
+
         for (int i = 0; i < N; i++) {
-            String[] strs = br.readLine().split("");
-            for (int j = 0; j < N; j++) {
-                graph[i][j] = strs[j];
-            }
+            graph[i] = br.readLine().toCharArray();
         }
-        //오른쪽 아래 swap
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                //오른쪽
+                //가로 교환
                 if (j + 1 < N) {
-                    swapRow(i, j);
-                    checkCandy(i, i, j, j + 1);
-                    swapRow(i, j);
+                    //SWAP
+                    char now = graph[i][j];
+                    graph[i][j] = graph[i][j + 1];
+                    graph[i][j+1] = now;
+
+                    checkMax(i, i, j, j + 1);
+                    //원상복구
+                    now = graph[i][j];
+                    graph[i][j] = graph[i][j + 1];
+                    graph[i][j+1] = now;
                 }
-                //아래
                 if (i + 1 < N) {
-                    swapCol(i, j);
-                    checkCandy(i, i+1, j, j);
-                    swapCol(i, j);
+                    //SWAP
+                    char now = graph[i][j];
+                    graph[i][j] = graph[i+1][j];
+                    graph[i+1][j] = now;
+
+                    checkMax(i, i+1, j, j);
+                    //원상복구
+                    now = graph[i][j];
+                    graph[i][j] = graph[i+1][j];
+                    graph[i+1][j] = now;
                 }
             }
         }
-        System.out.println(ans);
+
+        System.out.println(result);
     }
 
-    private static void checkCandy(int startRow, int endRow, int startCol, int endCol) {
+    private static void checkMax(int startRow, int endRow, int startCol, int endCol) {
         //가로 체크
         for (int i = startRow; i <= endRow; i++) {
             int cnt = 1;
             for (int j = 0; j < N-1; j++) {
-                if (graph[i][j].equals(graph[i][j + 1])) {
-                    cnt+=1;
+                char now = graph[i][j];
+                if (now == graph[i][j + 1]) {
+                    cnt++;
                 }else{
-                    cnt=1;
+                    cnt = 1;
                 }
-                ans = Math.max(ans, cnt);
+                result = Math.max(result,cnt);
             }
         }
-        //세로 체크
+
         for (int i = startCol; i <= endCol; i++) {
             int cnt = 1;
-            for (int j = 0; j < N - 1; j++) {
-                if (graph[j][i].equals(graph[j + 1][i])) {
-                    cnt+=1;
+            for (int j = 0; j < N-1; j++) {
+                char now = graph[j][i];
+                if (now == graph[j+1][i]) {
+                    cnt++;
                 }else{
-                    cnt=1;
+                    cnt = 1;
                 }
-                ans = Math.max(ans, cnt);
+                result = Math.max(result,cnt);
             }
         }
     }
-
-    private static void swapRow(int i, int j) {
-        String now = graph[i][j];
-        graph[i][j] = graph[i][j + 1];
-        graph[i][j+1] = now;
-    }
-    private static void swapCol(int i, int j) {
-        String now = graph[i][j];
-        graph[i][j] = graph[i+1][j];
-        graph[i+1][j] = now;
-    }
 }
+
+
+
