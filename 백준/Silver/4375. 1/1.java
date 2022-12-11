8245b6d36fd5818fb53f17bb0225fed9e3f6d50c
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2022-12-11 17:31:07 +0900

    [Silver III] Title: 1, Time: 196 ms, Memory: 17584 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.java
+++ 백준/Silver/4375. 1/1.java
@@ -1,26 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextInt()) {
             int N = sc.nextInt();
-            int num = 0;
-            int result = 0;
-            while (true) {
-                num = num * 10 + 1;
-                num %= N;
-                result++;
-                if (num == 0) {
-                    System.out.println(result);
+            int prevValue = 1%N;
+            for (int i = 1; ; i++) {
+                if(prevValue == 0){
+                    System.out.println(i);
                     break;
                 }
+                prevValue = (prevValue*10 +1)%N;
             }
         }
-        }
     }
-
-
+}
