9902f0e47af9aa5de63625f63a6336ae863ae6c7
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2023-01-10 22:28:42 +0900

    [Silver I] Title: 단지번호붙이기, Time: 152 ms, Memory: 14824 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,78 +1,75 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-public class Main {
-    static int N;
-    static int cnt = 0;
-    static List<Integer> arr = new ArrayList<>();
-    static int nX[] = {1,-1,0,0};
-    static int nY[] = {0, 0, 1, -1};
+class Main {
+    static int n;
     static int[][] graph;
+    static int[][] danjiGraph;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
 
-    static boolean[][] visited;
-    static class Node{
+    static class Edge {
         int x;
         int y;
 
-        Node(int x, int y) {
+        Edge(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
-    public static void main(String[] args) throws IOException {
 
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        graph = new int[N][N];
-        visited = new boolean[N][N];
-        for (int i = 0; i < N; i++) {
-            String[] strs = br.readLine().split("");
-            for (int j = 0; j < N; j++) {
-                graph[i][j] = Integer.parseInt(strs[j]);
-            }
-        }
-
-
-        int danzi = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if(graph[i][j] == 0) continue;
-                cnt = 0;
-                danzi++;
-                bfs(i, j);
-                arr.add(cnt);
-            }
-        }
-
-        System.out.println(danzi);
-        arr.sort(Comparator.naturalOrder());
-        for (int a : arr) {
-            System.out.println(a);
-        }
-    }
-
-    static void bfs(int x, int y) {
-        Queue<Node> q = new LinkedList<>();
-        Node n = new Node(x, y);
-        q.offer(n);
-        visited[x][y] = true;
+    static void bfs(int x, int y, int danji) {
+        Queue<Edge> q = new LinkedList<>();
+        danjiGraph[x][y] = danji;
+        q.offer(new Edge(x, y));
         while (!q.isEmpty()) {
-            Node node = q.poll();
-            graph[node.x][node.y] = 0;
-            cnt++;
-            
-            for (int i = 0; i < 4; i++) {
-                int nx = node.x + nX[i];
-                int ny = node.y + nY[i];
+            Edge now = q.poll();
 
-                if (nx >= 0 && nx < N && ny >= 0 && ny < N && graph[nx][ny] != 0 && !visited[nx][ny]) {
-                    q.offer(new Node(nx, ny));
-                    visited[nx][ny] = true;
+            for (int i = 0; i < 4; i++) {
+                int nX = now.x + dx[i];
+                int nY = now.y + dy[i];
+                if(nX < 0 || nX >= n || nY < 0 || nY >= n) continue;
+                if (danjiGraph[nX][nY] == 0 && graph[nX][nY] == 1) {
+                    danjiGraph[nX][nY] = danji;
+                    q.offer(new Edge(nX, nY));
                 }
             }
         }
     }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        graph = new int[n][n];
+        danjiGraph = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = Arrays.stream(br.readLine().split(""))
+                    .mapToInt(Integer::parseInt).toArray();
+        }
+        int danji = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(danjiGraph[i][j] == 0 && graph[i][j] == 1)
+                bfs(i, j, ++danji);
+            }
+        }
+        int[] ans = new int[danji];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (danjiGraph[i][j] != 0) {
+                    ans[danjiGraph[i][j]-1]++;
+                }
+            }
+        }
+        Arrays.sort(ans);
+        System.out.println(danji);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.println(ans[i]);
+        }
+    }
+
 }
+
+
+
+
