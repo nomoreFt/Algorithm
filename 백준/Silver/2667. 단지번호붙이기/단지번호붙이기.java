9280e1ec609afdc17d84a76c431a2578e0128370
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2023-01-10 22:59:03 +0900

    [Silver I] Title: 단지번호붙이기, Time: 148 ms, Memory: 14632 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,72 +2,76 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    static int n;
-    static int[][] graph;
-    static int[][] danjiGraph;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
+    static class Vertex{
+        int x,y;
 
-    static class Edge {
-        int x;
-        int y;
-
-        Edge(int x, int y) {
+        Vertex(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
 
-    static void bfs(int x, int y, int danji) {
-        Queue<Edge> q = new LinkedList<>();
-        danjiGraph[x][y] = danji;
-        q.offer(new Edge(x, y));
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static int[][] graph;
+    static int[][] danjiGraph;
+    static int n;
+    public static void bfs(Vertex now, int danji) {
+        Queue<Vertex> q = new LinkedList<>();
+        danjiGraph[now.x][now.y] = danji;
+        q.offer(now);
         while (!q.isEmpty()) {
-            Edge now = q.poll();
+            Vertex next = q.poll();
 
             for (int i = 0; i < 4; i++) {
-                int nX = now.x + dx[i];
-                int nY = now.y + dy[i];
-                if(nX < 0 || nX >= n || nY < 0 || nY >= n) continue;
-                if (danjiGraph[nX][nY] == 0 && graph[nX][nY] == 1) {
+                int nX = next.x + dx[i];
+                int nY = next.y + dy[i];
+                if (nX < 0 || nX >= n || nY < 0 || nY >= n) continue;
+                if (graph[nX][nY] == 1 && danjiGraph[nX][nY] == 0) {
                     danjiGraph[nX][nY] = danji;
-                    q.offer(new Edge(nX, nY));
+                    q.offer(new Vertex(nX, nY));
                 }
             }
         }
     }
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
 
         graph = new int[n][n];
         danjiGraph = new int[n][n];
+
         for (int i = 0; i < n; i++) {
-            graph[i] = Arrays.stream(br.readLine().split(""))
-                    .mapToInt(Integer::parseInt).toArray();
+            String[] strs = br.readLine().split("");
+            for (int j = 0; j < n; j++) {
+                graph[i][j] = Integer.parseInt(strs[j]);
+            }
         }
+
         int danji = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if(danjiGraph[i][j] == 0 && graph[i][j] == 1)
-                bfs(i, j, ++danji);
-            }
-        }
-        int[] ans = new int[danji];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (danjiGraph[i][j] != 0) {
-                    ans[danjiGraph[i][j]-1]++;
+                if (graph[i][j] == 1 && danjiGraph[i][j] == 0) {
+                    danji++;
+                    bfs(new Vertex(i, j), danji);
                 }
             }
         }
-        Arrays.sort(ans);
+
+        int[] ans = new int[danji];
         System.out.println(danji);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(danjiGraph[i][j] != 0)
+                ans[danjiGraph[i][j]-1]++;
+            }
+        }
+        Arrays.sort(ans);
         for (int i = 0; i < ans.length; i++) {
             System.out.println(ans[i]);
+
         }
     }
-
 }
 
 
