27a823d0869ae00bf589c4af1466ce2f20e366e5
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2023-01-10 23:31:33 +0900

    [Silver I] Title: 미로 탐색, Time: 196 ms, Memory: 16684 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,57 +1,61 @@
+import java.io.*;
 import java.util.*;
 
-public class Main{
-    static class Node{
-        int x;
-        int y;
-        public Node(int x, int y){
+class Main {
+    static class Vertex{
+        int x,y;
+
+        Vertex(int x, int y) {
             this.x = x;
             this.y = y;
+        }
+    }
+
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static int[][] graph;
+    static boolean[][] visited;
+    static int n,m;
+    public static void bfs() {
+        Queue<Vertex> q = new LinkedList<>();
+        q.offer(new Vertex(0, 0));
+        visited[0][0] = true;
+        while (!q.isEmpty()) {
+            Vertex now = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nX = now.x + dx[i];
+                int nY = now.y + dy[i];
+                if (nX < 0 || nX >= n || nY < 0 || nY >= m) continue;
+                if (!visited[nX][nY] && graph[nX][nY] == 1) {
+                    visited[nX][nY] = true;
+                    q.offer(new Vertex(nX, nY));
+                    graph[nX][nY] = graph[now.x][now.y] + 1;
+                }
+            }
 
         }
     }
-    static int[] dx = {1,-1,0,0};
-    static int[] dy = {0,0,1,-1};
-    static int n,m;
-    static int[][] graph;
-    static boolean[][] visited;
-    static void bfs(){
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(0,0));
-        visited[0][0] = true;
-        while(!q.isEmpty()){
-            Node node = q.poll();
-            for(int i = 0; i < 4; i++){
-                int nX = node.x + dx[i];
-                int nY = node.y + dy[i];
-                if(nX < 0 || nX >= n || nY < 0 || nY >= m) continue;
-                if(!visited[nX][nY] && graph[nX][nY] != 0){
-                    graph[nX][nY] = graph[node.x][node.y]+1;
-                    visited[nX][nY] = true;
-                    q.offer(new Node(nX,nY));
-                }
-            }
-        }
-    }
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String[] strs = sc.nextLine().split(" ");
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] strs = br.readLine().split(" ");
         n = Integer.parseInt(strs[0]);
         m = Integer.parseInt(strs[1]);
-        
-        visited = new boolean[n][m];
         graph = new int[n][m];
-        
-        for(int i = 0; i < n; i++){
-            strs = sc.nextLine().split("");
-            for(int j = 0; j < m; j++){
+        visited = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            strs = br.readLine().split("");
+            for (int j = 0; j < m; j++) {
                 graph[i][j] = Integer.parseInt(strs[j]);
             }
         }
 
         bfs();
-        System.out.println(graph[n-1][m-1]);
-        
 
+        System.out.println(graph[n-1][m-1]);
     }
 }
+
+
+
+
