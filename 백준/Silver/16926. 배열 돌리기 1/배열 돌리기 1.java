7e904f38d63eaadfa2292c5174623fdc2917b09e
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2023-01-30 23:51:53 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 1868 ms, Memory: 42312 KB -BaekjoonHub

---
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.sql.Array;
+import java.util.*;
+
+
+class Main {
+    static int n,m,r;
+    static int[][] graph;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] strs = br.readLine().split(" ");
+        n = Integer.parseInt(strs[0]);
+        m = Integer.parseInt(strs[1]);
+        r = Integer.parseInt(strs[2]);
+
+        graph = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            strs = br.readLine().split(" ");
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = Integer.parseInt(strs[j]);
+            }
+        }
+
+        int groupn = Math.min(n, m) / 2;
+        ArrayList<ArrayList<Integer>> groups = new ArrayList<>();
+        for (int k = 0; k < groupn; k++) {
+            ArrayList<Integer> group = new ArrayList<>();
+            for (int j = k; j < m - k -1; j++) {
+                group.add(graph[k][j]);
+            }
+            for (int i = k; i < n - 1 - k; i++) { //1 ~ n
+                group.add(graph[i][m - k - 1]);
+            }
+
+            for (int j = m - 1 - k; j > k; j--) {
+                group.add(graph[n - 1 - k][j]);
+            }
+            for (int i = n - 1 - k; i > k; i--) {
+                group.add(graph[i][k]);
+            }
+            groups.add(group);
+        }
+
+        for (int k = 0; k < groupn; k++) {
+            ArrayList<Integer> group = groups.get(k);
+            int len = group.size();
+            int index = r % len;
+
+            for (int j = k; j < m - k - 1; j++, index = (index+1)%len) {
+                graph[k][j] = group.get(index);
+            }
+            for (int i = k; i < n - 1 - k; i++, index = (index+1)%len) { //1 ~ n
+               graph[i][m - k - 1] = group.get(index);
+            }
+
+            for (int j = m - 1 - k; j > k; j--, index = (index+1)%len) {
+                graph[n - 1 - k][j] = group.get(index);
+            }
+            for (int i = n - 1 - k; i > k; i--, index = (index+1)%len) {
+                graph[i][k] = group.get(index);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(graph[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
