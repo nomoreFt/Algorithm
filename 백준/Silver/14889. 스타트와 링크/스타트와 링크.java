5e4b68efb9bcb7aee073392eed74ddff81c9a138
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2023-03-12 20:30:11 +0900

    [Silver II] Title: 스타트와 링크, Time: 360 ms, Memory: 22360 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,67 +1,54 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class Main {
-   static int[][] graph;
-   static ArrayList<Integer> arr = new ArrayList<>();
-   static boolean[] visited;
-   static int n;
-   static int min = Integer.MAX_VALUE;
-   static void diff() {
-    int team_start = 0;
-    int team_link = 0;
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (visited[i] == true && visited[j] == true) {
-                team_start += graph[i][j];
-                team_start += graph[j][i];
-            }
-            else if (visited[i] == false && visited[j] == false) {
-                team_link += graph[i][j];
-                team_link += graph[j][i];
-            }
-        }
-    }
-    int val = Math.abs(team_start - team_link);
-    if (val == 0) {
-        System.out.println(val);
-        System.exit(0);
-    }
-    min = Math.min(val, min);
-}
-static void combi(int idx, int count) {
-    if(count == n / 2) {
-        diff();
-        return;
-    }
-    for(int i = idx; i < n; i++) {
-        if(!visited[i]) {
-            visited[i] = true;	// 방문으로 변경
-            combi(i + 1, count + 1);	// 재귀 호출
-            visited[i] = false;	// 재귀가 끝나면 비방문으로 변경
-        }
-    }
-}
-    public static void main(String[] args) throws IOException{
+class Main {
+    static int N;
+    static int[][] S;
+    static ArrayList<Integer> start = new ArrayList<>();
+    static ArrayList<Integer> link = new ArrayList<>();
+    static int result = Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        String[] strs;
-        graph = new int[n][n];
-        visited = new boolean[n];
-        
-        for(int i = 0; i < n; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine()," ");
-            for(int j = 0; j < n; j++){
-                graph[i][j] = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine());
+        S = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            String[] strs = br.readLine().split(" ");
+            for (int j = 0; j < N; j++) {
+                S[i][j] = Integer.parseInt(strs[j]);
             }
         }
-        
-        combi(0,0);
-        System.out.println(min);
+        dfs(0, start, link);
+        System.out.println(result);
+    }
 
-  
-    
-        
-     
+    public static void dfs(int idx, ArrayList<Integer> start, ArrayList<Integer> link) {
+        if (idx == N) {
+            if(start.size() != N/2) return;
+            if(link.size() != N/2) return;
+            int startScore = 0;
+            int linkScore = 0;
+            for (int i = 0; i < start.size(); i++) {
+                for (int j = 0; j < start.size(); j++) {
+                    startScore += S[start.get(i)][start.get(j)];
+                }
+            }
+            for (int i = 0; i < link.size(); i++) {
+                for (int j = 0; j < link.size(); j++) {
+                    linkScore += S[link.get(i)][link.get(j)];
+                }
+            }
+            result = Math.min(result,Math.abs(startScore - linkScore));
+            return;
+        }
+        if(idx > N) return;
+        start.add(idx);
+        dfs(idx + 1, start, link);
+        start.remove(start.size()-1);
+        link.add(idx);
+        dfs(idx + 1, start, link);
+        link.remove(link.size()-1);
     }
 }
