1f8d328a567eacd0ba8b03e3b22be1aa2d1e9715
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2023-04-27 22:31:47 +0900

    [Gold III] Title: 파일 합치기, Time: 708 ms, Memory: 22596 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.java
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -1,57 +1,49 @@
-import java.util.*;
-import java.io.*;
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main{
     static int T;
     static int[] arr;
     static int[][] dist;
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         T = Integer.parseInt(br.readLine());
 
         while (T-- > 0) {
-            int K = Integer.parseInt(br.readLine());
-            arr = new int[K+1];
-            dist = new int[K + 1][K + 1];
+            int k = Integer.parseInt(br.readLine());
+            arr = new int[k + 1];
+            dist = new int[k + 1][k + 1];
+
             String[] strs = br.readLine().split(" ");
-            for (int i = 1; i <= K; i++) {
-                arr[i] = Integer.parseInt(strs[i-1]);
+            for (int i = 1; i <= k; i++) {
+                arr[i] = Integer.parseInt(strs[i - 1]);
                 Arrays.fill(dist[i], -1);
             }
 
-
-            System.out.println(go(1,K));
+            System.out.println(solve(1, k));
         }
+
+
     }
 
-    private static int go(int start, int end) {
+    private static int solve(int start, int end) {
+        if(start == end) return 0;
+        if(dist[start][end] != -1) return dist[start][end];
 
-        if(start == end){
-            return 0;
-        }
-
-        if (dist[start][end] != -1) {
-            return dist[start][end];
-        }
-
-        int ans = -1;
+        int ret = Integer.MAX_VALUE;
         int sum = 0;
         for (int k = start; k <= end; k++) {
             sum += arr[k];
         }
-
-        for (int k = start; k <= end - 1; k++) {
-            int temp = go(start,k) + go(k+1,end) + sum;
-            if (ans == -1 || ans > temp) {
-                ans = temp;
-            }
+        for (int i = start; i < end; i++) {
+            int temp = solve(start, i) + solve(i + 1, end) + sum;
+            ret = Math.min(ret, temp);
         }
 
-        dist[start][end] = ans;
-        return ans;
-
-
-
+        return dist[start][end] = ret;
 
     }
+
 }
