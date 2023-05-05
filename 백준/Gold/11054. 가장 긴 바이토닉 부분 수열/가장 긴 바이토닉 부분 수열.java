8b5f5b87f7860f10f2be5b88edf25471299fcfd2
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2023-05-05 18:00:53 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 156 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -1,46 +1,47 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
-import java.util.OptionalInt;
-import java.util.stream.IntStream;
 
-public class Main {
+class Main {
+    static int[] A;
+    static int[] D;
+    static int[] D2;
     static int N;
-    static int[] arr;
-    static int[] dpL;
-    static int[] dpR;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        String[] s = br.readLine().split(" ");
-        arr = new int[N + 1];
-        dpL = new int[N + 1];
-        dpR = new int[N + 1];
+        D = new int[N];
+        D2 = new int[N];
+        A = new int[N];
+
+        String[] strs = br.readLine().split(" ");
         for (int i = 0; i < N; i++) {
-            arr[i + 1] = Integer.parseInt(s[i]);
-            dpL[i + 1] = 1;
-            dpR[i + 1] = 1;
+            A[i] = Integer.parseInt(strs[i]);
         }
 
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j < i; j++) {
-                if (arr[j] < arr[i]) {
-                    dpL[i] = Math.max(dpL[i], dpL[j] + 1);
+        for (int i = 0; i < N; i++) {
+            D[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (A[i] > A[j] && D[i] < D[j] + 1) {
+                    D[i] = D[j] + 1;
                 }
             }
         }
-        for (int i = N; i >= 1; i--) {
-            for (int j = N; j > i; j--) {
-                if (arr[j] < arr[i]) {
-                    dpR[i] = Math.max(dpR[i], dpR[j]+1);
+        for (int i = N-1; i >= 0; i--) {
+            D2[i] = 1;
+            for (int j = N-1; j > i; j--) {
+                if (A[i] > A[j] && D2[i] < D2[j] + 1) {
+                    D2[i] = D2[j] + 1;
                 }
             }
         }
-        int max = 0;
-        for (int i = 1; i <= N; i++) {
-            max = Math.max(dpL[i] + dpR[i] - 1, max);
+
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            ans =  Math.max(ans, D[i] + D2[i] -1);
         }
-        System.out.println(max);
+        System.out.println(ans);
     }
 }
+
+
+
