019eb415e34d74ad66b82e335c4b6170df5ec7b6
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2023-05-10 23:07:27 +0900

    [Gold V] Title: 인구 이동, Time: 564 ms, Memory: 296820 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -1,88 +1,109 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-
-class Position{
-    int x;
-    int y;
-    public Position(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-}
-
 public class Main {
-	public static int N,L,R;
-	public static int[][] graph = new int[51][51];
-	public static int[][] union = new int[51][51];
-	  public static int[] dx = {-1, 0, 1, 0};
-	    public static int[] dy = {0, -1, 0, 1};
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		L = sc.nextInt();
-		R = sc.nextInt();
-		//초기셋팅
-		for(int i = 0 ; i <N; i++) {
-			for (int j = 0 ; j < N; j++) {
-				graph[i][j] = sc.nextInt();
-			}
-		}
-		int totalCnt = 0;
-		while(true) {
-			int index = 0;
-			
-			for(int i = 0 ; i < N; i++) {
-				Arrays.fill(union[i],-1);
-			}
-			
-			for(int i = 0 ; i < N; i++) {
-				for(int j = 0 ; j < N; j++) {
-					if(union[i][j] == -1) {
-						bfs(i,j,index);
-						index += 1;
-					}
-				}
-			}
-			
-			if(index == N * N) break;
-			totalCnt += 1;
-			
-		}
-		System.out.println(totalCnt);
-	}
-	public static void bfs(int x, int y, int index) {
-		ArrayList<Position> united = new ArrayList<>();
-		united.add(new Position(x,y));
-		Queue<Position> q = new LinkedList<>();
-		q.offer(new Position(x,y));
-		union[x][y] = index;
-		int sum = graph[x][y];
-		int count = 1;
-		while(!q.isEmpty()) {
-			Position pos = q.poll();
-			x = pos.x;
-			y = pos.y;
-			for(int i = 0; i < 4; i++) {
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-				if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-				if(union[nx][ny] == -1) {
-					int gap = Math.abs(graph[pos.x][pos.y] - graph[nx][ny]);
-					if(gap >= L && gap <= R) {
-						union[nx][ny] = index;
-						q.offer(new Position(nx,ny));
-						united.add(new Position(nx,ny));
-						sum += graph[nx][ny];
-						count += 1;
-				}
-				}
-				
-			}
-		}
-		for(int i = 0 ; i < united.size(); i++) {
-			Position pos = united.get(i);
-			graph[pos.x][pos.y] = sum/count;
-		}
-	}
+    static int N;
+    static int L,R;
+    static int[][] graph;
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {1,-1,0,0};
+    static class Node{
+        int x;
+        int y;
+        Node(int x,int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        L = Integer.parseInt(input[1]);
+        R = Integer.parseInt(input[2]);
 
+        graph = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            input = br.readLine().split(" ");
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = Integer.parseInt(input[j]);
+            }
+        }
+
+        int result = 0;
+        while (true) {
+            if (go()) {
+                result++;
+            } else {
+                break;
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    public static boolean go() {
+        boolean ok = true;
+
+        int[][] unions = new int[N][N];
+        int unionNum = 0;
+
+
+
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (unions[i][j] == 0) {
+                    unionNum++;
+                    findUnion(unions, unionNum, new Node(i,j));
+                }
+            }
+        }
+
+        if(unionNum == N * N) return false;
+
+
+        return ok;
+    }
+
+    public static void findUnion(int[][] unions,int unionNum, Node start) {
+        Queue<Node> q = new LinkedList<>();
+        q.offer(start);
+        unions[start.x][start.y] = unionNum;
+
+        Queue<Node> qUnion = new LinkedList<>();
+        qUnion.offer(start);
+        int unionCnt = 1;
+        int sum = graph[start.x][start.y];
+
+        while (!q.isEmpty()) {
+            Node now = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nX = now.x + dx[i];
+                int nY = now.y + dy[i];
+
+                if(nX < 0 || nX >= N || nY < 0 || nY >= N) continue;
+                if(unions[nX][nY] != 0) continue;
+
+                int diff = Math.abs(graph[now.x][now.y] - graph[nX][nY]);
+                if(diff >= L && diff <= R){
+                    q.offer(new Node(nX, nY));
+                    unions[nX][nY] = unionNum;
+                    qUnion.offer(new Node(nX, nY));
+                    unionCnt++;
+                    sum += graph[nX][nY];
+                }
+            }
+        }
+
+        if(unionCnt == 1) return;
+        int val = sum / unionCnt;
+        while (!qUnion.isEmpty()) {
+            Node now = qUnion.poll();
+            graph[now.x][now.y] = val;
+        }
+    }
 }
