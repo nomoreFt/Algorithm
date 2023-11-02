87782aef23814d32fb092745a4d14c08c8f23c13
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2023-11-02 15:26:36 +0900

    [Gold IV] Title: 테트로미노, Time: 640 ms, Memory: 113828 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,71 +1,99 @@
 import java.io.*;
 import java.util.*;
 
-
 class Main {
-    static int N, M;
+    static int N,M;
     static int[][] graph;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
     static boolean[][] visited;
     static int MAX = Integer.MIN_VALUE;
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
+
+    static class Node{
+        int x,y;
+        Node(int x,int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    //남,북,동,서
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        N = Integer.parseInt(inputs[0]);
+        M = Integer.parseInt(inputs[1]);
+
         graph = new int[N][M];
         visited = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                graph[i][j] = sc.nextInt();
-            }
+            graph[i] = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
         }
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                go(i, j, 0, 0);
-                if (j + 2 < M) {
-                    int temp = graph[i][j] + graph[i][j + 1] + graph[i][j + 2];
-                    if (i - 1 >= 0) {
-                        int temp2 = temp + graph[i - 1][j + 1];
-                        MAX = Math.max(MAX, temp2);
-                    }
-                    if (i + 1 < N) {
-                        int temp2 = temp + graph[i + 1][j + 1];
-                        MAX = Math.max(MAX, temp2);
-                    }
-                }
-                if (i + 2 < N) {
-                    int temp = graph[i][j] + graph[i + 1][j] + graph[i + 2][j];
-                    if (j - 1 >= 0) {
-                        int temp2 = temp + graph[i + 1][j - 1];
-                        MAX = Math.max(MAX, temp2);
-                    }
-                    if (j + 1 < M) {
-                        int temp2 = temp + graph[i + 1][j + 1];
-                        MAX = Math.max(MAX, temp2);
-                    }
-                }
+                visited[i][j] = true;
+                dfs(new Node(i,j), 1, graph[i][j]);
+                visited[i][j] = false;
+
+                calculateT(new Node(i,j));
             }
         }
+
         System.out.println(MAX);
+
+
+    }
+    //남북동서
+    static void calculateT(Node now) {
+        //ㅓ
+        if(now.y - 1 >= 0 && now.x + 1 < N && now.x - 1 >= 0) {
+            MAX = Math.max(MAX,
+                    graph[now.x][now.y] + graph[now.x][now.y - 1] + graph[now.x + 1][now.y] + graph[now.x - 1][now.y]);
+        }
+        //ㅏ
+        if(now.y + 1 < M && now.x + 1 < N && now.x - 1 >= 0)
+            MAX = Math.max(MAX,
+                    graph[now.x][now.y] + graph[now.x][now.y + 1] + graph[now.x + 1][now.y] + graph[now.x - 1][now.y]);
+        //ㅗ
+        if(now.x - 1 >= 0 && now.y - 1 >= 0 && now.y + 1 < M)
+            MAX= Math.max(MAX,
+                    graph[now.x][now.y] + graph[now.x - 1][now.y] + graph[now.x][now.y - 1] + graph[now.x][now.y + 1]);
+
+        //ㅜ
+
+        if(now.x + 1 < N && now.y - 1 >= 0 && now.y + 1 < M)
+        MAX = Math.max(MAX,
+                graph[now.x][now.y] + graph[now.x + 1][now.y] + graph[now.x][now.y - 1] + graph[now.x][now.y + 1]);
 
     }
 
-    private static void go(int x, int y, int sum, int depth) {
-        if (depth == 4) {
+    static void dfs(Node now, int idx, int sum){
+        if(idx == 4){
             MAX = Math.max(MAX, sum);
             return;
         }
-        if(x < 0 || x >= N || y < 0 || y >= M) return;
-        if(visited[x][y]) return;
-        visited[x][y] = true;
+
         for (int i = 0; i < 4; i++) {
-            int nX = x + dx[i];
-            int nY = y + dy[i];
-            go(nX, nY, sum + graph[x][y], depth + 1);
+            int nX = now.x + dx[i];
+            int nY = now.y + dy[i];
+
+            if(nX < 0 || nX >= N || nY < 0 || nY >= M) continue;
+            if(visited[nX][nY]) continue;
+
+            visited[nX][nY] = true;
+            dfs(new Node(nX, nY), idx + 1, sum + graph[nX][nY]);
+            visited[nX][nY] = false;
         }
-        visited[x][y] = false;
     }
+
+
+
+
+
+
 }
