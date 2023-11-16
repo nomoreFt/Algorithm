c3edac141cb885ccff9f69900cd2c08914717d30
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2023-11-16 14:42:04 +0900

    [Silver I] Title: 에너지 모으기, Time: 172 ms, Memory: 17932 KB -BaekjoonHub

--- 백준/Silver/16198. 에너지 모으기/에너지 모으기.java
+++ 백준/Silver/16198. 에너지 모으기/에너지 모으기.java
@@ -1,36 +1,40 @@
 import java.io.*;
 import java.util.*;
 
-
 class Main {
-    static int N;
-    static ArrayList<Integer> W = new ArrayList<>();
-    static int ans = Integer.MIN_VALUE;
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
 
-        for (int i = 0; i < N; i++) {
-            W.add(sc.nextInt());
+    static int N;
+    static ArrayList<Integer> goos;
+    static int answer = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        goos = new ArrayList<>();
+        String[] inputs = br.readLine().split(" ");
+        for(int i = 0; i < N; i++) {
+            goos.add(Integer.parseInt(inputs[i]));
         }
 
-        go(0, W);
-        System.out.println(ans);
+        solution(0,goos);
+
+        System.out.println(answer);
+
     }
 
-    private static void go(int sum, ArrayList<Integer> w) {
-        int size = w.size();
-        if (size == 2) {
-            ans = Math.max(ans, sum);
+    public static void solution(int sum,ArrayList<Integer> list) {
+        int size = list.size();
+        if(size == 2) {
+            answer = Math.max(answer,sum);
             return;
         }
 
-        for (int i = 1; i < size - 1; i++) {
-            int temp = w.get(i);
-            int sumTemp = w.get(i - 1) * w.get(i + 1);
-            w.remove(i);
-            go(sum + sumTemp, w);
-            w.add(i,temp);
+        for(int i = 1; i < size-1; i++) {
+            int temp = list.get(i);
+            int tempSum = list.get(i-1) * list.get(i+1);
+            list.remove(i);
+            solution(sum+tempSum,list);
+            list.add(i,temp);
         }
 
     }
