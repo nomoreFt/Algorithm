d1966baa0c01755f805d5276a7384a2b144f98ff
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2024-05-23 21:30:46 +0900

    Time: 0 ms (100%), Space: 44.4 MB (56.42%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -4,15 +4,18 @@ class Solution {
             return nums.length;
         }
 
-        int index = 2; // 초기값은 2, 세 번째 원소부터 확인 시작
+        int index = 2;
 
         for (int i = 2; i < nums.length; i++) {
-            if (nums[i] != nums[index - 2]) { // 현재 원소가 index-2 원소와 다르면
-                nums[index] = nums[i]; // 현재 원소를 index 위치에 저장
+            if (nums[i] != nums[index - 2]) {
+                nums[index] = nums[i];
                 index++;
             }
         }
 
-        return index; // 중복을 제거한 후의 길이 반환
+        // 새 배열로 잘라내기
+        int[] reuslt = Arrays.copyOfRange(nums, 0, index);
+        return reuslt.length;
     }
+
 }
