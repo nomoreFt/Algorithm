3527e7e406360d4fdba8ff0b3d998262f21b708a
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2024-06-07 18:33:58 +0900

    Time: 3 ms (99.65%), Space: 68.7 MB (28.51%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,34 +10,30 @@
  */
 public class Solution {
     public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) {
-            return true;
-        }
+       ListNode fast = head, slow = head;
+       
+       while(fast != null && fast.next != null){
+        fast = fast.next.next;
+        slow = slow.next;
+       }
 
-        ListNode slow = head;
-        ListNode fast = head;
-        Stack<Integer> stack = new Stack<>();
+       if(fast != null){
+        slow = slow.next;
+       }
 
-        // 첫 번째 절반을 스택에 저장
-        while (fast != null && fast.next != null) {
-            stack.push(slow.val);
-            slow = slow.next;
-            fast = fast.next.next;
-        }
+       ListNode rev = null;
+       while(slow != null){
+        ListNode next = slow.next;
+        slow.next = rev;
+        rev = slow;
+        slow = next;
+       }
 
-        // 리스트의 길이가 홀수일 경우, 중간 요소를 건너뜀
-        if (fast != null) {
-            slow = slow.next;
-        }
-
-        // 스택에서 값을 꺼내며 비교
-        while (slow != null) {
-            if (stack.pop() != slow.val) {
-                return false;
-            }
-            slow = slow.next;
-        }
-
-        return true;
+       while(rev != null){
+        if(rev.val != head.val) return false;
+        rev = rev.next;
+        head = head.next;
+       }
+       return true;
     }
 }
