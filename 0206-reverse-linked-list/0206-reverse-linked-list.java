d48341cbee0c7a172bc8dfdf14d0a9379085c25d
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2024-06-20 00:06:04 +0900

    Time: 0 ms (100%), Space: 42.6 MB (19.9%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -12,12 +12,12 @@ class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode tail = null;
         ListNode current = head;
-        while(current != null) {
-            ListNode temp = current.next; // 맨 앞을 제외한 노드들 임시 저장
-            current.next = tail; // 맨 앞 노드를 tail에 붙인다.
-            tail = current; // slow에 붙였기 때문에 tail을 slow 로 치환
-            current = temp; //slow는 계속해서 잘라서 써야하기 때문에 temp로 치환
+        while(current != null){
+            ListNode temp = current.next;
+            current.next = tail;
+            tail = current;
+            current = temp;
         }
-        return tail;
+               return tail;
     }
 }
