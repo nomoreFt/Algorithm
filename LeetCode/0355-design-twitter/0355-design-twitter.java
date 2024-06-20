aac3b9cc829342945e34d2bad01b13c3cd9df938
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2024-06-20 18:31:53 +0900

    Time: 13 ms (5.6%), Space: 42.3 MB (6.54%) - LeetHub

--- LeetCode/0355-design-twitter/0355-design-twitter.java
+++ LeetCode/0355-design-twitter/0355-design-twitter.java
@@ -2,15 +2,14 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 class Twitter {
-    class Tweet implements Comparable<Tweet> {
+    class Tweet {
         int id;
         LocalDateTime createdTime;
-        int creatorId;
+        Tweet next;
 
-        @Override
-        public int compareTo(Tweet o) {
-            // 최신순 정렬
-            return o.createdTime.compareTo(this.createdTime);
+        Tweet(int id) {
+            this.id = id;
+            this.createdTime = LocalDateTime.now();
         }
     }
 
@@ -18,12 +17,19 @@ class Twitter {
         int id;
         Set<Integer> followers;
         Set<Integer> followees;
-        PriorityQueue<Tweet> tweets;
+        Tweet tweetHead;
 
-        public User() {
+        User(int id) {
+            this.id = id;
             followers = new HashSet<>();
             followees = new HashSet<>();
-            tweets = new PriorityQueue<>();
+            tweetHead = null;
+        }
+
+        void postTweet(int tweetId) {
+            Tweet newTweet = new Tweet(tweetId);
+            newTweet.next = tweetHead;
+            tweetHead = newTweet;
         }
     }
 
@@ -34,52 +40,48 @@ class Twitter {
     }
 
     public void postTweet(int userId, int tweetId) {
-        // userId에 해당하는 User에 tweetId 추가
-        if (!userMap.containsKey(userId)) {
-            userMap.put(userId, new User());
-        }
+        userMap.putIfAbsent(userId, new User(userId));
         User user = userMap.get(userId);
-        Tweet newTweet = new Tweet();
-        newTweet.id = tweetId;
-        newTweet.createdTime = LocalDateTime.now();
-        newTweet.creatorId = userId;
-        user.tweets.add(newTweet);
+        user.postTweet(tweetId);
     }
 
     public List<Integer> getNewsFeed(int userId) {
-        // follow한 사람들과 자기가 쓴 글 중 최근 10개
         List<Integer> result = new ArrayList<>();
         if (!userMap.containsKey(userId)) {
             return result;
         }
 
         User user = userMap.get(userId);
-        PriorityQueue<Tweet> feed = new PriorityQueue<>(user.tweets);
+        PriorityQueue<Tweet> feed = new PriorityQueue<>((a, b) -> b.createdTime.compareTo(a.createdTime));
 
+        addTweetsToFeed(feed, user.tweetHead);
         for (int followeeId : user.followees) {
-            User followee = userMap.get(followeeId);
-            if (followee != null && followee.tweets != null) {
-                feed.addAll(followee.tweets);
+            if (userMap.containsKey(followeeId)) {
+                addTweetsToFeed(feed, userMap.get(followeeId).tweetHead);
             }
         }
 
-        for (int i = 0; i < 10 && !feed.isEmpty(); i++) {
+        int count = 0;
+        while (!feed.isEmpty() && count < 10) {
             result.add(feed.poll().id);
+            count++;
         }
 
         return result;
     }
 
+    private void addTweetsToFeed(PriorityQueue<Tweet> feed, Tweet head) {
+        while (head != null) {
+            feed.offer(head);
+            head = head.next;
+        }
+    }
+
     public void follow(int followerId, int followeeId) {
-        // followerId가 followeeId를 follow
         if (followerId == followeeId) return;
-        
-        if (!userMap.containsKey(followerId)) {
-            userMap.put(followerId, new User());
-        }
-        if (!userMap.containsKey(followeeId)) {
-            userMap.put(followeeId, new User());
-        }
+
+        userMap.putIfAbsent(followerId, new User(followerId));
+        userMap.putIfAbsent(followeeId, new User(followeeId));
 
         User follower = userMap.get(followerId);
         User followee = userMap.get(followeeId);
@@ -89,7 +91,6 @@ class Twitter {
     }
 
     public void unfollow(int followerId, int followeeId) {
-        // followerId가 followeeId를 unfollow
         if (followerId == followeeId || !userMap.containsKey(followerId) || !userMap.containsKey(followeeId)) return;
 
         User follower = userMap.get(followerId);
