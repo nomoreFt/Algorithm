bc0168a8c2b4fc9a51470562b95f1fac53830b59
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2024-06-28 13:32:13 +0900

    Time: 78 ms (5.49%), Space: 44.6 MB (5.04%) - LeetHub

---
+++ LeetCode/0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -0,0 +1,28 @@
+class Solution {
+    public Set<Character> toSortedSet(String s) {
+        Set<Character> set = new TreeSet<>((o1, o2) -> {
+            if (o1.equals(o2)) {
+                return 0;
+            } else if (o1 > o2) {
+                return 1;
+            } else {
+                return -1;
+            }
+        });
+        
+        for (char c : s.toCharArray()) {
+            set.add(c);
+        }
+        return set;
+    }
+    public String removeDuplicateLetters(String s) {
+        for(char c : toSortedSet(s)){
+            String suffix = s.substring(s.indexOf(c));
+            if(toSortedSet(s).equals(toSortedSet(suffix))){
+                return c + removeDuplicateLetters(suffix.replace(String.valueOf(c),""));
+            }
+        }
+
+        return "";
+    }
+}
