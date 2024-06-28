bf2272c80a5bbd531e31f4b04134e89880a43f80
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2024-06-28 15:03:49 +0900

    Time: 2 ms (94.84%), Space: 41.8 MB (93.33%) - LeetHub

--- LeetCode/0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ LeetCode/0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,33 +1,61 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-        // 각 문자가 등장하는 마지막 인덱스를 저장하는 배열
-        int[] lastIndex = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            lastIndex[s.charAt(i) - 'a'] = i;
+       int[] lastIdx = new int[26];
+       for(int i = 0; i < s.length(); i++){
+        lastIdx[s.charAt(i) - 'a'] = i;
+       }
+
+       boolean[] visited = new boolean[26];
+       Deque<Character> stack = new ArrayDeque<>();
+       
+       for(int i = 0; i < s.length(); i++){
+        char target = s.charAt(i);
+    
+        if(visited[target - 'a']) continue;
+
+        while(!stack.isEmpty() && stack.peek() > target && lastIdx[stack.peek() - 'a'] > i){
+            visited[stack.pop() - 'a'] = false;
         }
-        
-        boolean[] visited = new boolean[26]; // 결과 문자열에 문자가 포함되었는지 여부를 추적하는 배열
-        Deque<Character> stack = new ArrayDeque<>(); // 결과를 저장할 스택
-        
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (visited[c - 'a']) continue; // 이미 결과에 포함된 문자는 건너뜁니다.
-            
-            // 스택이 비어있지 않고, 스택의 최상단 문자가 현재 문자보다 크며, 스택의 최상단 문자가 뒤에 다시 등장한다면
-            while (!stack.isEmpty() && stack.peek() > c && lastIndex[stack.peek() - 'a'] > i) {
-                visited[stack.pop() - 'a'] = false; // 스택에서 제거하고 방문 여부를 갱신합니다.
-            }
-            
-            stack.push(c); // 현재 문자를 스택에 추가합니다.
-            visited[c - 'a'] = true; // 방문 여부를 갱신합니다.
-        }
-        
-        // 스택을 문자열로 변환하여 결과를 반환합니다.
-        StringBuilder result = new StringBuilder();
-        for (char c : stack) {
-            result.append(c);
-        }
-        
-        return result.reverse().toString();
+
+        stack.push(target);
+        visited[target - 'a'] = true;
+
+       }
+
+       StringBuilder sb = new StringBuilder();
+       for(char c : stack){
+        sb.append(c);
+       }
+
+       return sb.reverse().toString();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
