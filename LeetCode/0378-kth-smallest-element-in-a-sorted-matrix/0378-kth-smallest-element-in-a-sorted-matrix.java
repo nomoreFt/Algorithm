7ee3d4c4561ddeed643d3e81ef838fb9b01d075a
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2024-06-30 18:21:52 +0900

    Time: 24 ms (9.04%), Space: 47.8 MB (84.37%) - LeetHub

--- LeetCode/0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ LeetCode/0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,35 +1,11 @@
 class Solution {
-        public int kthSmallest(int[][] matrix, int k) {
-        if (matrix == null || k < 1) {
-            throw new IllegalArgumentException("Invalid input");
-        }
-
-        int n = matrix.length;
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        Set<String> set = new HashSet<>();
-        
-        // 초기 힙과 집합 설정
-        minHeap.offer(new int[]{matrix[0][0], 0, 0});
-        set.add("0,0");
-        
-        while (k > 1) {
-            int[] top = minHeap.poll();
-            int row = top[1];
-            int col = top[2];
-
-            if (col + 1 < n && !set.contains(row + "," + (col + 1))) {
-                minHeap.offer(new int[]{matrix[row][col + 1], row, col + 1});
-                set.add(row + "," + (col + 1));
-            }
-
-            if (row + 1 < n && !set.contains((row + 1) + "," + col)) {
-                minHeap.offer(new int[]{matrix[row + 1][col], row + 1, col});
-                set.add((row + 1) + "," + col);
-            }
-
-            k--;
-        }
-
-        return minHeap.peek()[0];
+    public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int[] mat : matrix)
+            for(int m : mat)
+                pq.offer(m);
+        while(--k != 0)
+            pq.poll();
+        return pq.peek();
     }
 }
