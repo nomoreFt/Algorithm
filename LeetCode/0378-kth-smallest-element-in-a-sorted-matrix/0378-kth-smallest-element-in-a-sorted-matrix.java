23f7b29a82f6854959954738d16a21d9c1bdc662
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2024-06-30 18:21:35 +0900

    Time: 70 ms (5%), Space: 51.8 MB (6.33%) - LeetHub

--- LeetCode/0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ LeetCode/0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,16 +1,35 @@
 class Solution {
-    public int kthSmallest(int[][] matrix, int k) {
-        int n = matrix.length;
-        int[] answer = new int[n * n];
-        
-        int answerIdx = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                answer[answerIdx++] = matrix[i][j];
-            }
+        public int kthSmallest(int[][] matrix, int k) {
+        if (matrix == null || k < 1) {
+            throw new IllegalArgumentException("Invalid input");
         }
+
+        int n = matrix.length;
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        Set<String> set = new HashSet<>();
         
-        Arrays.sort(answer);
-        return answer[k - 1];
+        // 초기 힙과 집합 설정
+        minHeap.offer(new int[]{matrix[0][0], 0, 0});
+        set.add("0,0");
+        
+        while (k > 1) {
+            int[] top = minHeap.poll();
+            int row = top[1];
+            int col = top[2];
+
+            if (col + 1 < n && !set.contains(row + "," + (col + 1))) {
+                minHeap.offer(new int[]{matrix[row][col + 1], row, col + 1});
+                set.add(row + "," + (col + 1));
+            }
+
+            if (row + 1 < n && !set.contains((row + 1) + "," + col)) {
+                minHeap.offer(new int[]{matrix[row + 1][col], row + 1, col});
+                set.add((row + 1) + "," + col);
+            }
+
+            k--;
+        }
+
+        return minHeap.peek()[0];
     }
 }
