c49d84263d456fb1bb3673ebac22872cd7a38987
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2024-06-30 19:35:50 +0900

    Time: 0 ms (100%), Space: 49.8 MB (23.35%) - LeetHub

--- LeetCode/0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ LeetCode/0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,32 +1,36 @@
-//행, 열 둘 다 asc 오름차순 정렬이라는 점 키포인트..
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         int n = matrix.length;
-        int low = matrix[0][0], high = matrix[n - 1][n - 1];
+        int low = matrix[0][0];
+        int high = matrix[n-1][n-1];
+
         while(low <= high){
-            int mid = low + (high - low) / 2; //오버플로우 없이 중간 값 찾기 쌉가능
-            int count = getLowerNum(matrix, mid);
-            if(count < k) low = mid + 1;//k번째 작은 값은 mid보다 큰 범위에 있다.
-            else high = mid - 1;//k번째 작은 값은 mid보다 작은 범위에 있다.
+            int mid = low + (high - low) /2;//오버플로우가 나지 않는다.
+            int count = getLowCount(matrix, mid);
+            
+            if(count < k){//k번째는 더 오른쪽
+                low = mid + 1;
+            }else{//k번째는 더 왼쪽
+                high = mid - 1;
+            }    
         }
         return low;
     }
-    private int getLowerNum(int[][] matrix, int num){
+    private int getLowCount(int[][] matrix, int mid){
+        int count = 0;
         int n = matrix.length;
-        int row = n - 1, col = 0;
-        int res = 0;
-
-        //맨 밑의 시작 값부터 시작해서 matrix값이 더 크면 올려(정렬 되어있으니까)
-        //같거나 작으면 해당 위는 다 같거나 작은 값이니 개수 추가하고 오른쪽으로 이동
-        //row는 위로 (--) 만 가고, col은 오른쪽 (++) 으로만 간다.
+        
+        int row = n-1;
+        int col = 0;
+        
         while(row >= 0 && col < n){
-            if(matrix[row][col] > num) //위로 올려
+            if(matrix[row][col] <= mid){
+                count += row + 1;
+                col++;
+            }else{
                 row--;
-            else{
-                res += row + 1;//개수라 rowIdx + 1.세로로 +
-                col++;//오른쪽으로 이동
             }
         }
-        return res;
+        return count;
     }
 }
