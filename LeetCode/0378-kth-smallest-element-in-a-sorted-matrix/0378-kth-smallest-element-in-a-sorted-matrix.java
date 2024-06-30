10da5789cdaa2db3cf21f2d9f59b4e3df50e06c2
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2024-06-30 18:08:52 +0900

    Time: 20 ms (27.72%), Space: 47.6 MB (89.42%) - LeetHub

--- LeetCode/0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ LeetCode/0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,16 +1,30 @@
 class Solution {
-    public int kthSmallest(int[][] matrix, int k) {
+        public int kthSmallest(int[][] matrix, int k) {
         int n = matrix.length;
-        int[] answer = new int[n * n];
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
         
-        int answerIdx = 0;
+        // 첫 열의 모든 원소를 힙에 추가
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                answer[answerIdx++] = matrix[i][j];
+            minHeap.offer(new int[]{matrix[i][0], i, 0});
+        }
+        
+        int count = 0;
+        while (!minHeap.isEmpty()) {
+            int[] curr = minHeap.poll();
+            int value = curr[0];
+            int row = curr[1];
+            int col = curr[2];
+            
+            if (++count == k) {
+                return value;
+            }
+            
+            if (col + 1 < n) {
+                minHeap.offer(new int[]{matrix[row][col + 1], row, col + 1});
             }
         }
         
-        Arrays.sort(answer);
-        return answer[k - 1];
+        throw new IllegalArgumentException("Invalid input.");
     }
+
 }
