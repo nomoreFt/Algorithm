24451619a6d48e1cb083da7ebc277bd5923d9546
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2024-06-30 15:32:52 +0900

    Time: 2 ms (54.84%), Space: 41.5 MB (37.89%) - LeetHub

--- LeetCode/0020-valid-parentheses/0020-valid-parentheses.java
+++ LeetCode/0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,26 +1,26 @@
-import java.util.*;
-
 class Solution {
     public boolean isValid(String s) {
         Deque<Character> stack = new ArrayDeque<>();
-        
-        Map<Character, Character> table = new HashMap<>() {{
+        Map<Character,Character> table = new HashMap<>() {{
             put(')','(');
             put('}','{');
             put(']','[');
         }};
 
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            if(!table.containsKey(c)){
-                stack.push(c);
-            } else {
-                if(stack.isEmpty() || table.get(c) != stack.pop()) {
-                    return false;
-                }
+        //stack에 시작 브라킷 넣으면서 진행, stack.peek()에 닫힌 브라킷 상대역이 아니면 false
+        int len = s.length();
+        for(int i = 0; i < len; i++){
+            char now = s.charAt(i);
+            if(!table.containsKey(now)){//열린 브래킷이면 무조건 담음
+                stack.push(now);
+            }else{//끝맺음 브라킷
+                if(stack.isEmpty()) return false;
+                if(table.get(now) != stack.peek()) return false;
+                //같은 짝궁이면 pop
+                stack.pop();
             }
         }
-        
+
         return stack.isEmpty();
     }
 }
