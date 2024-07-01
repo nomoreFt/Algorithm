c3be297c8138288a59db05be3db63cfb8c091dc6
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2024-07-01 10:17:05 +0900

    Time: 2 ms (94.78%), Space: 42.1 MB (70.37%) - LeetHub

--- LeetCode/0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ LeetCode/0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,33 +1,37 @@
 class Solution {
-    int alphaCnt = 26;
     public String removeDuplicateLetters(String s) {
-     int len = s.length();
-     int[] lastIdx = new int[alphaCnt];
-     for(int i = 0; i < len; i++){
+       int[] lastIdx = new int[26];
+       for(int i = 0; i < s.length(); i++){
         lastIdx[s.charAt(i) - 'a'] = i;
-     }
+       }//각 문자가 등장하는 마지막 idx 저장 (앞에 나왔다면 순서상 불필요하면 제거하기 위해)
 
-     boolean[] visited = new boolean[alphaCnt];
-     Deque<Character> stack = new ArrayDeque<>();
-
-     for(int i = 0; i < len; i++){
+       boolean[] visited = new boolean[26];
+       Deque<Character> stack = new ArrayDeque<>();
+       
+       for(int i = 0; i < s.length(); i++){
         char target = s.charAt(i);
+    
         if(visited[target - 'a']) continue;
-        while(!stack.isEmpty() 
-        && stack.peek() > target
-        && lastIdx[stack.peek() - 'a'] > i){
+
+        while(!stack.isEmpty() && stack.peek() > target && lastIdx[stack.peek() - 'a'] > i){
             visited[stack.pop() - 'a'] = false;
         }
 
         stack.push(target);
         visited[target - 'a'] = true;
-     }
 
-     StringBuilder sb = new StringBuilder();
-     for(char c : stack){
+       }
+
+       StringBuilder sb = new StringBuilder();
+       for(char c : stack){
         sb.append(c);
-     }
+       }
 
-     return sb.reverse()toString();
+       return sb.reverse().toString();
     }
 }
+
+
+
+
+
