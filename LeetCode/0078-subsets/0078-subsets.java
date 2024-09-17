f03ac3653d3baa35eca7338992bfe29ca8b786ae
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2024-09-17 21:56:59 +0900

    Time: 1 ms (53.02%), Space: 42.7 MB (33.61%) - LeetHub

--- LeetCode/0078-subsets/0078-subsets.java
+++ LeetCode/0078-subsets/0078-subsets.java
@@ -1,16 +1,18 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        dfs(result, new ArrayList<>(), nums, 0);
-        return result;
+        List<List<Integer>> results = new ArrayList<>();
+        dfs(results, nums, 0, new ArrayDeque<>());
+
+        return results;
     }
-    
-    public void dfs(List<List<Integer>> result, List<Integer> tempList, int[] nums, int start){
-        result.add(new ArrayList<>(tempList));
-        for(int i = start; i < nums.length; i++){
-            tempList.add(nums[i]);
-            dfs(result, tempList, nums, i + 1);
-            tempList.remove(tempList.size() - 1);
+
+    public void dfs(List<List<Integer>> results, int[] nums, int idx, ArrayDeque<Integer> path){
+        results.add(new ArrayList<>(path));
+        
+        for(int i = idx; i < nums.length; i++){
+            path.add(nums[i]);
+            dfs(results, nums, i + 1, path);
+            path.removeLast();
         }
     }
 }
