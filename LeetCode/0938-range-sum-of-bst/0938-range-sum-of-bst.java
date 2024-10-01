e09f808131c9eeee58fa370c863891375eb76736
Hyunwoo Kim <hyunwoo.kim1108@gmail.com>
2024-10-01 15:40:23 +0900

    Time: 0 ms (100%), Space: 51.1 MB (48.64%) - LeetHub

--- LeetCode/0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ LeetCode/0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -14,15 +14,18 @@
  * }
  */
 class Solution {
-    int sum = 0;
-    public int rangeSumBST(TreeNode root, int low, int high) {
-        if(root == null) return 0;
 
-        rangeSumBST(root.left, low, high);
-        rangeSumBST(root.right, low, high);
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) return 0;
+
+        // 현재 노드의 값이 high보다 크면 왼쪽 서브트리만 탐색
+        if (root.val > high) return rangeSumBST(root.left, low, high);
         
-        if(low <= root.val && root.val <= high) return sum += root.val;
-        return sum;
+        // 현재 노드의 값이 low보다 작으면 오른쪽 서브트리만 탐색
+        if (root.val < low) return rangeSumBST(root.right, low, high);
+
+        // 현재 노드의 값이 범위 내에 있다면, 현재 노드의 값과 좌우 서브트리의 값을 합산
+        return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
     }
     
 }
